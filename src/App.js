3459489315be78f267ba8beaea9639788e8f6f3c
Daniel Francisco <deny.jo@hotmail.com>
2022-07-09 13:47:50 +0100

    Add basic full page with arrow key to navigate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-function App() {
+import React from "react";
+import { useEffect } from "react";
+
+function App () {
+  let currentSection = 1;
+
+  const nextSection = () => {
+    currentSection += 1;
+    window.location.href = `#section${currentSection}`;
+  }
+
+  const previousSection = () => {
+    currentSection -= 1;
+    window.location.href = `#section${currentSection}`;
+  }
+ 
+  useEffect(() => {
+    document.addEventListener('keydown', detectKeyDown, true);
+  });
+
+  const detectKeyDown = (event) => {
+    switch(event.key) {
+      case 'ArrowDown':
+        event.preventDefault();
+        nextSection();
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        previousSection();
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <div id="section1" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ff0000'}}>
+        <h1 style={{margin: '0px'}}>Section 1</h1>
+      </div>
+      <div id="section2" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#00ff00'}}>
+        <h1 style={{margin: '0px'}}>Section 2</h1>
+      </div>
+      <div id="section3" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0000ff'}}>
+        <h1 style={{margin: '0px'}}>Section 3</h1>
+      </div>
     </div>
   );
 }
