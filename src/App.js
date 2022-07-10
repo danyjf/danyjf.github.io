098233159f3bc7c97b6c22b1c309b0e20d2025de
Daniel Francisco <deny.jo@hotmail.com>
2022-07-11 00:20:12 +0100

    Add section horizontal component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FullPageScroller from "./components/FullPageScroller";
 import Section from "./components/Section";
+import SectionHorizontal from "./components/SectionHorizontal";
 
 function App () {
   return (
@@ -15,6 +16,17 @@ function App () {
         <Section style={{display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0798ec'}}>
           <h1 style={{margin: '0px'}}>Section 3</h1>
         </Section>
+        <SectionHorizontal>
+          <Section style={{display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#282c34'}}>
+            <h1 style={{margin: '0px'}}>Section 4.1</h1>
+          </Section>
+          <Section style={{display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ff5f45'}}>
+            <h1 style={{margin: '0px'}}>Section 4.2</h1>
+          </Section>
+          <Section style={{display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0798ec'}}>
+            <h1 style={{margin: '0px'}}>Section 4.3</h1>
+          </Section>
+        </SectionHorizontal>
       </FullPageScroller>
     </div>
   );
---
+++ src/components/SectionHorizontal.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+
+class SectionHorizontal extends React.Component {
+  render() {  
+    return (
+      <div className="SectionHorizontal">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default SectionHorizontal;
