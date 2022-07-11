cc5c00c19ef576ae66a30c210355b27e6ea0dce9
Daniel Francisco <deny.jo@hotmail.com>
2022-07-11 15:10:47 +0100

    Clear project

--- src/components/Section.js
+++
@@ -1,9 +0,0 @@
-﻿function Section(props) {
-  return (
-    <div className="Section" style={{...props.style, height: '100vh'}}>
-      {props.children}
-    </div>
-  );
-}
-
-export default Section;
