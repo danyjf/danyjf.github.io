4cca5f599178a3683dc203af86bb799d4563bca3
Daniel Francisco <deny.jo@hotmail.com>
2022-07-11 12:28:27 +0100

    Horizontal slide on click

--- src/components/SectionHorizontal.js
+++ src/components/SectionHorizontal.js
@@ -1,8 +1,18 @@
 ﻿import React from "react";
+import $ from "jquery";
 
 class SectionHorizontal extends React.Component {
-  
   render() {
+    document.onclick = function() {
+      let elem = $("#horizontal");
+      let previousLeft = parseInt(elem.css('left'));
+      previousLeft = previousLeft * (100 / document.documentElement.clientWidth);
+
+      elem.animate({
+        left: previousLeft - 100 + 'vw'
+      }, 500);
+    }
+
     const childrenWithCorrectStyle = React.Children.map(this.props.children, child => {
       if(React.isValidElement(child)) {
         return React.cloneElement(child, {style: {...child.props.style, width: 100.0 / this.props.children.length + '%', float: 'left'}});
@@ -11,7 +21,15 @@ class SectionHorizontal extends React.Component {
     });
 
     return (
-      <div className="SectionHorizontal" style={{width: this.props.children.length * 100 + '%'}}>
+      <div 
+        id="horizontal" 
+        className="SectionHorizontal" 
+        style={{
+          position: 'absolute', 
+          width: this.props.children.length * 100 + '%', 
+          zIndex: '-1'
+        }}
+      >
         {childrenWithCorrectStyle}
       </div>
     );
