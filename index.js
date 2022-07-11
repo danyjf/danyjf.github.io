a7280fb87d0fa2d2444f2a9037daf1df39495e05
Daniel Francisco <deny.jo@hotmail.com>
2022-07-11 18:40:37 +0100

    Add basic portfolio

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function() {
+  $('#projectsPageButton').on('click', function() {
+    $('#pageContent').html(`
+      <hr>
+
+      <div class='row'>
+        <div class='col'>
+          
+        </div>
+      </div>
+    `);
+  });
+
+  $('#careerPageButton').on('click', function() {
+    $('#pageContent').html(`
+      <hr>
+
+      <div class='row'>
+        <div class='col'>
+          <h1>University of Aveiro</h1>
+          <h1>2019 - 2022</h1>
+          <p>I received a bachelors degree in Computer Science from the University of Aveiro.</p>
+        </div>
+      </div>
+    `);
+  });
+});
