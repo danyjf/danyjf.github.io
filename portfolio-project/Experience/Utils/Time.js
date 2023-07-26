4e568043d39403173c2d81977d261f67f906f887
Daniel Francisco <deny.jo@hotmail.com>
2023-07-26 20:37:09 +0100

    Refactor project by separating code into different files

---
+++ portfolio-project/Experience/Utils/Time.js
@@ -0,0 +1,23 @@
+﻿import { EventEmitter } from "events"
+
+export default class Time extends EventEmitter {
+    constructor() {
+        super();
+        this.start = Date.now();
+        this.current = this.start;
+        this.elapsed = 0;           // time since the beginning of the program 
+        this.delta = 16;            // time since the last frame
+        
+        this.update();
+    }
+
+    update() {
+        const currentTime = Date.now();
+        this.delta = currentTime - this.current;
+        this.current = currentTime;
+        this.elapsed = this.current - this.start;
+
+        this.emit("update");
+        window.requestAnimationFrame(() => this.update());
+    }
+}
---
+++ portfolio-project/Experience/World/World.js
@@ -0,0 +1,17 @@
+﻿import * as THREE from "three";
+
+import Experience from "../Experience";
+
+import Room from "./Room";
+
+export default class World {
+    constructor() {
+        this.experience = new Experience();
+
+        this.room = new Room();
+    }
+
+    resize() {}
+
+    update() {}
+}
