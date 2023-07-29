10660bb51274d5f1bbad35139058c4daef5b5d41
Daniel Francisco <deny.jo@hotmail.com>
2023-07-29 23:20:23 +0100

    Add basic movement

--- portfolio-project/Game/World/Player.js
+++ portfolio-project/Game/World/Player.js
@@ -6,19 +6,43 @@ export default class Player {
     constructor() {
         this.game = new Game();
         this.scene = this.game.scene;
+        this.speed = 0.1;
     
+        document.addEventListener("keydown", this.onDocumentKeyDown.bind(this), false);
+
         this.setModel();
     }
 
     setModel() {
         const geometry = new THREE.BoxGeometry(0.15, 0.15, 0.15); 
         const material = new THREE.MeshStandardMaterial({color: 0xcccccc}); 
-        const player = new THREE.Mesh(geometry, material); 
-        player.position.y = 0.075;
-        this.scene.add(player);
+        this.player = new THREE.Mesh(geometry, material); 
+        this.player.position.y = 0.075;
+        this.scene.add(this.player);
+    }
+
+    onDocumentKeyDown(event) {
+        switch (event.code) {
+            case "KeyW":
+            case "ArrowUp":
+                this.player.position.x -= this.speed;
+                break;
+            case "KeyS":
+            case "ArrowDown":
+                this.player.position.x += this.speed;
+                break;
+            case "KeyA":
+            case "ArrowLeft":
+                this.player.position.z += this.speed;
+                break;
+            case "KeyD":
+            case "ArrowRight":
+                this.player.position.z -= this.speed;
+                break;
+        }
     }
 
     update() {
-        // take care of the controls
+        
     }
 }
