80fc0d03dc78e1e1bc69e0b751e92b06cd7841a3
Daniel Francisco <deny.jo@hotmail.com>
2024-07-18 12:23:54 +0100

    Create hero section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 ﻿import './styles/modern-normalize.scss'
 import './styles/style.scss';
 import './styles/components/header.scss';
+import './styles/components/hero.scss';
+import './styles/components/about.scss';
 import './styles/utils.scss';
