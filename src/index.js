f68fa2182ac096ce55194282b2d4d87d0bd18ee4
Daniel Francisco <deny.jo@hotmail.com>
2024-07-19 13:01:01 +0100

    Start creating project section

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './styles/style.scss';
 import './styles/components/header.scss';
 import './styles/components/hero.scss';
 import './styles/components/about.scss';
+import './styles/components/projects.scss';
 import './styles/utils.scss';
