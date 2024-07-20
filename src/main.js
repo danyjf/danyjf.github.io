535d9bb31da7f70a1fcd92f91fd9cf0933d84f15
Daniel Francisco <deny.jo@hotmail.com>
2024-07-20 20:50:04 +0100

    Add project details pop up

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './styles/components/header.scss';
 import './styles/components/hero.scss';
 import './styles/components/about.scss';
 import './styles/components/projects.scss';
+import './styles/components/project-details.scss';
 import './styles/components/contact.scss';
 import './styles/components/footer.scss';
 import './styles/components/mobile-nav.scss';
@@ -12,7 +13,9 @@ import './styles/utils.scss';
 import mobileNav from './utils/mobile-nav';
 import darkMode from './utils/dark-mode';
 import lazyLoading from './utils/lazy-loading';
+import projectDetails from './utils/project-details';
 
 mobileNav();
+projectDetails();
 darkMode();
 lazyLoading();
---
+++ src/utils/project-details.js
@@ -0,0 +1,25 @@
+﻿function projectDetails() {
+    const viewProjectBtns = document.querySelectorAll('.projects__project-btn');
+    const closeBtns = document.querySelectorAll('.project-details__close-btn');
+    let projectDetails = null;
+
+    viewProjectBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            projectDetails = document.querySelector('#project-details-' + btn.value);
+            projectDetails.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';
+        });
+    });
+
+    closeBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            if (projectDetails)
+            {
+                projectDetails.style.display = 'none';
+                document.body.style.overflowY = 'auto';
+            }
+        });
+    });
+}
+
+export default projectDetails;
