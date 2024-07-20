40d1045e11716d3899a86b6ca0716b3d8f6d505d
Daniel Francisco <deny.jo@hotmail.com>
2024-07-20 01:44:08 +0100

    Add logic for dark and light mode toggle and store last selected option

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,11 +1,20 @@
 ﻿function darkMode() {
     const themeToggleButtons = document.querySelectorAll('#theme-toggle');
 
-    themeToggleButtons.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            document.body.classList.toggle('light-mode');
-        });
-    });
+    const theme = localStorage.getItem('theme');
+    theme && document.body.classList.add(theme);
+
+    function handleThemeToggle() {
+        document.body.classList.toggle('light-mode');
+        if(document.body.classList.contains('light-mode')) {
+            localStorage.setItem('theme', 'light-mode');
+        } else {
+            localStorage.removeItem('theme');
+            document.body.removeAttribute('class');
+        }
+    };
+
+    themeToggleButtons.forEach((btn) => btn.addEventListener('click', handleThemeToggle));
 }
 
 export default darkMode;
