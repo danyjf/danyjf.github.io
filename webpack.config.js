3a6f68d8364bd0949587065c5c4051d675908a08
Daniel Francisco <deny.jo@hotmail.com>
2024-07-21 21:49:53 +0100

    Add video and images to project details

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
             directory: path.resolve(__dirname, 'dist')
         },
         port: 3000,
+        watchFiles: ['src/*.html'],
         open: true,
         hot: true,
         compress: true,
@@ -40,6 +41,15 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(mp4|webm)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
                     }
                 }
             },
