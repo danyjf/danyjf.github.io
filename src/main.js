a4ba49de1b945e0f32f3db72aa9c53fb0cababb0
Daniel Francisco <deny.jo@hotmail.com>
2024-07-22 16:48:27 +0100

    Add modal to zoom in on images

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 ﻿import './styles/modern-normalize.scss'
 import './styles/style.scss';
+import './styles/components/modal.scss'
 import './styles/components/header.scss';
 import './styles/components/hero.scss';
 import './styles/components/about.scss';
@@ -10,11 +11,20 @@ import './styles/components/footer.scss';
 import './styles/components/mobile-nav.scss';
 import './styles/utils.scss';
 
+import './assets/images/hero.jpg';
+import './assets/images/placeholder_square.jpg';
+import './assets/images/placeholder_square_red.jpg';
+import './assets/images/placeholder_square_green.jpg';
+import './assets/images/placeholder_square_blue.jpg';
+import './assets/videos/placeholder_video.webm';
+
 import mobileNav from './utils/mobile-nav';
 import darkMode from './utils/dark-mode';
 import lazyLoading from './utils/lazy-loading';
 import projectDetails from './utils/project-details';
+import modal from './utils/modal';
 
+modal();
 mobileNav();
 projectDetails();
 darkMode();
---
+++ src/utils/modal.js
@@ -0,0 +1,24 @@
+﻿function modal() {
+    const modalBtns = document.querySelectorAll('.modal__btn');
+    const modal = document.querySelector('.modal');
+    const modalContent = document.querySelector('.modal__content');
+    const modalCloseBtn = document.querySelector('.modal__close-btn')
+
+    modalBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            modalContent.src = btn.src;
+            modalContent.alt = btn.alt;
+            modal.style.display = 'flex';
+        });
+    });
+
+    modal.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modalContent.src = '';
+            modalContent.alt = '';
+            modal.style.display = 'none';
+        }
+    });
+}
+
+export default modal;
