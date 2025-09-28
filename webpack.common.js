3298713355408777aa8bd3179a970a1eca4cce96
Daniel Francisco <deny.jo@hotmail.com>
2025-09-28 16:48:34 +0100

    Fix hamburger menu staying on when expanding the width of the page

--- webpack.common.js
+++ webpack.common.js
@@ -1,71 +1,76 @@
-﻿const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+﻿const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        bundle: path.resolve(__dirname, 'src/main.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-        clean: true,
-        assetModuleFilename: '[name][ext]'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    // 'style-loader',
-                    // 'css-loader',
-                    // 'sass-loader'
-                    MiniCssExtractPlugin.loader,
-                    // 'style-loader', 
-                    'css-loader', 
-                    'sass-loader'
-                ]
+  entry: {
+    bundle: path.resolve(__dirname, "src/main.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // 'style-loader',
+          // 'css-loader',
+          // 'sass-loader'
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              api: "modern-compiler",
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.(mp4|webm)$/i,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]'
-                    }
-                }
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            title: 'Daniel Francisco - Portfolio',
-            filename: 'index.html',
-            template: 'src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: false, // Enable to remove warnings about conflicting order
-        }),
-    ]
-}
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(mp4|webm)$/i,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: "Daniel Francisco - Portfolio",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+};
